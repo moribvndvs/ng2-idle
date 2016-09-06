01bc6de78feb43d395e8b7b7fb4644d05a7efa86
Andy Brandt <andyb@unipointsoftware.com>
2016-09-06 14:27:19 -0500

    chore(deps): update to angular 2.0.0-rc.6

--- test-main.js
+++ test-main.js
@@ -27,7 +27,27 @@ System.config({
   defaultJSExtensions: true,
   map: {
     'rxjs': 'node_modules/rxjs',
-    '@angular': 'node_modules/@angular'
+
+    // angular bundles
+    '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js',
+    '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
+    '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/http': 'node_modules/@angular/http/bundles/http.umd.js',
+    '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
+    '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js',
+
+    // angular testing umd bundles
+    '@angular/core/testing': 'node_modules/@angular/core/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'node_modules/@angular/common/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'node_modules/@angular/compiler/bundles/compiler-testing.umd.js',
+    '@angular/platform-browser/testing': 'node_modules/@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+    '@angular/http/testing': 'node_modules/@angular/http/bundles/http-testing.umd.js',
+    '@angular/router/testing': 'node_modules/@angular/router/bundles/router-testing.umd.js',
+    '@angular/forms/testing': 'node_modules/@angular/forms/bundles/forms-testing.umd.js'
+
   },
   packages: {
     '@angular/core': {
@@ -63,8 +83,7 @@ Promise.all([
   var testing = providers[0];
   var testingBrowser = providers[1];
 
-  testing.setBaseTestProviders(testingBrowser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
-    testingBrowser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+  testing.TestBed.initTestEnvironment(testingBrowser.BrowserDynamicTestingModule, testingBrowser.platformBrowserDynamicTesting());
 
 }).then(function() {
   return Promise.all(
