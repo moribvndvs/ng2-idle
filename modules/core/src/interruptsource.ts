c94fc9a93a27a716b3598bcdb4fca7df083bf6ee
Steven Liekens <steven.liekens@gmail.com>
2018-02-09 13:49:39 +0100

    fix(InterruptSource): attach outside Angular

--- modules/core/src/interruptsource.ts
+++ modules/core/src/interruptsource.ts
@@ -1,6 +1,6 @@
-import {EventEmitter} from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
-import {InterruptArgs} from './interruptargs';
+import { InterruptArgs } from './interruptargs';
 
 /*
  * A base for classes that act as a source for interrupts.
@@ -11,13 +11,21 @@ export abstract class InterruptSource {
   public onInterrupt: EventEmitter<InterruptArgs> = new EventEmitter<InterruptArgs>();
 
   constructor(
-      protected attachFn?: (source: InterruptSource) => void,
-      protected detachFn?: (source: InterruptSource) => void) {}
+    protected attachFn?: (source: InterruptSource) => void,
+    protected detachFn?: (source: InterruptSource) => void) { }
 
   /*
    * Attaches to the specified events on the specified source.
    */
   attach(): void {
+    // If the current zone is the 'angular' zone (a.k.a. NgZone) then re-enter this method in its parent zone
+    // The parent zone is usually the '<root>' zone but it can also be 'long-stack-trace-zone' in debug mode
+    // In tests, the current zone is typically a 'ProxyZone' created by async/fakeAsync (from @angular/core/testing)
+    if (Zone.current.get('isAngularZone') === true) {
+      Zone.current.parent.run(() => this.attach());
+      return;
+    }
+
     if (!this.isAttached && this.attachFn) {
       this.attachFn(this);
     }
