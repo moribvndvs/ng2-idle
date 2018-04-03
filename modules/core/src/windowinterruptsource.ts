118a3cc47e42a5fc52daa84df8587a0567e39c23
fadelguy <32434449+fadelguy@users.noreply.github.com>
2018-04-03 09:49:26 +0300

    Check is from browser

--- modules/core/src/windowinterruptsource.ts
+++ modules/core/src/windowinterruptsource.ts
@@ -1,10 +1,12 @@
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import {EventTargetInterruptOptions, EventTargetInterruptSource} from './eventtargetinterruptsource';
 
 /*
  * An interrupt source on the Window object.
  */
 export class WindowInterruptSource extends EventTargetInterruptSource {
-  constructor(events: string, options?: number | EventTargetInterruptOptions) {
-    super(window, events, options);
+  constructor(events: string, options?: number | EventTargetInterruptOptions, @Inject(PLATFORM_ID) private platformId: any) {
+    super(isPlatformBrowser(platformId) ? window : null, events, options);
   }
 }
