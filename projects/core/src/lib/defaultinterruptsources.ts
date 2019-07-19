bd172cfec05b544acd6123a5dc792beff847e331
Mike Grabski <me@mikegrabski.com>
2019-07-19 04:25:24 -0400

    fix(public-api): retain parity with exports from old index

--- projects/core/src/lib/defaultinterruptsources.ts
+++ projects/core/src/lib/defaultinterruptsources.ts
@@ -2,19 +2,6 @@ import { DocumentInterruptSource } from './documentinterruptsource';
 import { EventTargetInterruptOptions } from './eventtargetinterruptsource';
 import { StorageInterruptSource } from './storageinterruptsource';
 
-export * from './idle';
-export * from './interruptargs';
-export * from './interruptsource';
-export * from './eventtargetinterruptsource';
-export * from './documentinterruptsource';
-export * from './windowinterruptsource';
-export * from './storageinterruptsource';
-export * from './keepalivesvc';
-export * from './idleexpiry';
-export * from './simpleexpiry';
-export * from './localstorage';
-export * from './localstorageexpiry';
-
 export function createDefaultInterruptSources(
   options?: EventTargetInterruptOptions
 ) {
@@ -28,5 +15,3 @@ export function createDefaultInterruptSources(
 }
 
 export const DEFAULT_INTERRUPTSOURCES: any[] = createDefaultInterruptSources();
-
-export { NgIdleModule } from './module';
--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -3,10 +3,16 @@
  */
 
 export * from './lib/idle';
-export * from './lib/keepalivesvc';
-export * from './lib/module';
+export * from './lib/interruptargs';
+export * from './lib/interruptsource';
 export * from './lib/eventtargetinterruptsource';
+export * from './lib/documentinterruptsource';
 export * from './lib/windowinterruptsource';
 export * from './lib/storageinterruptsource';
+export * from './lib/keepalivesvc';
+export * from './lib/idleexpiry';
 export * from './lib/simpleexpiry';
-export * from './lib/documentinterruptsource';
+export * from './lib/localstorage';
+export * from './lib/localstorageexpiry';
+export * from './lib/module';
+export * from './lib/defaultinterruptsources';
