aa71e5714177706892c5d6424c2b3de27f70a22d
Mike Grabski <me@mikegrabski.com>
2019-07-23 16:59:46 -0400

    chore(packaging): try to synchronize core as peerDependency for core

--- scripts/write-package-json.js
+++ scripts/write-package-json.js
@@ -2,21 +2,21 @@ const fs = require('fs');
 const path = require('path');
 const util = require('./util');
 
-const basePkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const corePkg = JSON.parse(fs.readFileSync('./dist/core/package.json', 'utf8'));
 
-var modules = util.getFolders('./modules');
+const projects = util.getFolders('./projects');
 
-modules.map(function(module) {
-  var src = path.join('./modules', module, 'package.json');
-  var dest = path.join('./dist', module, 'package.json');
+projects.map(function(project) {
+  const dest = path.join('./dist', project, 'package.json');
+  const pkg = JSON.parse(fs.readFileSync(src));
+  let changed = false;
 
-  var pkg = JSON.parse(fs.readFileSync(src))
-  pkg.version = basePkg.version;
-  pkg.peerDependencies = Object.assign({}, basePkg.dependencies);
-
-  if (module === 'keepalive') {
+  if (project === 'keepalive') {
     pkg.peerDependencies['@ng-idle/core'] = '^' + basePkg.version;
+    changed = true;
   }
 
-  fs.writeFileSync(dest, JSON.stringify(pkg, null, 2), 'utf-8');
+  if (changed) {
+    fs.writeFileSync(dest, JSON.stringify(pkg, null, 2), 'utf-8');
+  }
 });
