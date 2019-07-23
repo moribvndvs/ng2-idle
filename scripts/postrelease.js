68a73098ef14cc11c040db439b10496dcf3af909
Mike Grabski <me@mikegrabski.com>
2019-07-23 18:52:57 -0400

    build: copy readme and license info into dist before publishing

--- scripts/postrelease.js
+++ scripts/postrelease.js
@@ -3,10 +3,7 @@ const path = require('path');
 const util = require('./util');
 
 const lerna = JSON.parse(fs.readFileSync('./lerna.json', 'utf8'));
-
-const projects = util.getFolders('./projects');
-
-projects.map(function(project) {
+const updateProject = project => {
   const dest = path.join('./dist', project, 'package.json');
   const pkg = JSON.parse(fs.readFileSync(dest));
   let changed = false;
@@ -19,4 +16,10 @@ projects.map(function(project) {
   if (changed) {
     fs.writeFileSync(dest, JSON.stringify(pkg, null, 2), 'utf-8');
   }
+};
+
+const projects = util.getFolders('./projects');
+
+projects.map(project => {
+  updateProject(project);
 });
