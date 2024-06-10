372400c9673597dfc4d13e12b048e77f08eaa127
BhuiyanSaif18 <bhuiyansaif18@gmail.com>
2024-06-10 18:44:20 +0600

    feat: 🎸 (core & keepalive)
    Exposed providers array from module files

--- projects/keepalive/src/lib/module.ts
+++ projects/keepalive/src/lib/module.ts
@@ -1,7 +1,15 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { KeepaliveSvc, NgIdleModule } from '@ng-idle/core';
+import { ModuleWithProviders, NgModule, Provider } from '@angular/core';
+import { KeepaliveSvc, NgIdleModule, provideNgIdle } from '@ng-idle/core';
 
 import { Keepalive } from './keepalive';
+
+export function provideNgIdleKeepalive(): Provider[] {
+  return [
+    provideNgIdle(),
+    Keepalive,
+    { provide: KeepaliveSvc, useExisting: Keepalive }
+  ];
+};
 
 @NgModule({ imports: [NgIdleModule.forRoot()] })
 export class NgIdleKeepaliveModule {
